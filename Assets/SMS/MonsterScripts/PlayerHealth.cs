0bbc627c6bb3e215a849632951fed053d00e0a81
mmmean <165157018+mmmean@users.noreply.github.com>
2025-07-16 16:19:58 +0900

    임시 부활 동기화 완료,

--- Assets/SMS/MonsterScripts/PlayerHealth.cs
+++ Assets/SMS/MonsterScripts/PlayerHealth.cs
@@ -6,8 +6,9 @@ public class PlayerHealth : LivingEntity
 {
     public float current_health;
     private Animator playerAnimator; // 플레이어의 애니메이터
-    PhotonView pv;
+    public PhotonView pv;
     private PlayerController playerController; // 플레이어 움직임 컴포넌트
+    private PlayerInput playerInput;
 
     private void Awake()
     {
@@ -15,6 +16,7 @@ public class PlayerHealth : LivingEntity
         playerController = GetComponent<PlayerController>();
         current_health = startingHealth;
         pv = GetComponent<PhotonView>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     protected override void OnEnable()
@@ -30,6 +32,8 @@ public class PlayerHealth : LivingEntity
     public override void Resurrection()
     {
         base.Resurrection();
+        pv.RPC("SetDeadState", RpcTarget.All, false);
+        pv.RPC("RPC_TriggerPlayerResurrection", RpcTarget.All);
     }
 
     // 데미지 처리
@@ -49,12 +53,18 @@ public class PlayerHealth : LivingEntity
     {
         // LivingEntity의 Die() 실행 (사망 적용)
         base.Die();
+        dead = true;
         Debug.Log(dead);
 
         // 사망 시 애니메이션 및 컴포넌트 비활성화는 모든 클라이언트에서 동기화
+        pv.RPC("SetDeadState", RpcTarget.All, true);
         pv.RPC("RPC_TriggerPlayerDie", RpcTarget.All);
     }
-
+    [PunRPC]
+    public void SetDeadState(bool isDead)
+    {
+        dead = isDead;
+    }
     [PunRPC]
     void RPC_TriggerPlayerHit()
     {
@@ -66,43 +76,48 @@ public class PlayerHealth : LivingEntity
     {
         playerAnimator.ResetTrigger("Hit");
         playerAnimator.SetTrigger("Die");
-        /* if (!dead) // 중복 호출 방지
-         {
-             dead = true;
-             playerAnimator.SetTrigger("Die");
-         }*/
+        
 
         // 움직임 중지
-        if (playerController != null)
-        {
-            playerController.canMove = false;
-        }
+        
 
         // 조작비활성화 (그 클라이언트만)
         if (pv.IsMine)
         {
-            /*if (TryGetComponent(out PlayerInput playerInput))
-            {
-                playerInput.enabled = false;
-            }*/
-            if (TryGetComponent(out Hammer hammer))
-            {
-                hammer.enabled = false;
-            }
-            if (TryGetComponent(out PenAttack penAttack))
-            {
-                Debug.Log("펜 어택 false");
-                penAttack.enabled = false;
-            }
-            if (TryGetComponent(out Pen_Skill_1 penSkill1))
-            {
-                penSkill1.enabled = false;
-            }
-            if (TryGetComponent(out Pen_Skill_2 penSkill2))
-            {
-                penSkill2.enabled = false;
-            }
             
+            playerInput.actions.FindAction("Move")?.Disable();
+            playerInput.actions.FindAction("Attack")?.Disable();
+            playerInput.actions.FindAction("Skill1")?.Disable();
+            playerInput.actions.FindAction("Skill2")?.Disable();
+            playerInput.actions.FindAction("Jump")?.Disable();
+            playerInput.actions.FindAction("Sprint")?.Disable();
+            playerInput.actions.FindAction("Look")?.Disable();
+            playerInput.actions.FindAction("Resurrection")?.Disable();
         }
+    }
+    [PunRPC]
+    void RPC_TriggerPlayerResurrection()
+    {
+        
+        dead = false;
+        playerAnimator.ResetTrigger("Die");
+        playerAnimator.SetTrigger("Resurrection");
+        // 조작활성화 (그 클라이언트만)
+        if (pv.IsMine)
+        {
+            Debug.Log($"[RPC] {name} 부활 RPC 실행됨. IsMine: {pv.IsMine}");
+            Debug.Log($"죽어있는가? : {dead}");
+            playerInput.actions.FindAction("Attack")?.Enable();
+            playerInput.actions.FindAction("Skill1")?.Enable();
+            playerInput.actions.FindAction("Skill2")?.Enable();
+            playerInput.actions.FindAction("Jump")?.Enable();
+            playerInput.actions.FindAction("Sprint")?.Enable();
+            playerInput.actions.FindAction("Look")?.Enable();
+            playerInput.actions.FindAction("Move")?.Enable();
+            playerInput.actions.FindAction("Resurrection")?.Enable();
+
+            Debug.Log(playerInput.actions.FindAction("Move"));
+        }
+        
     }
 }
